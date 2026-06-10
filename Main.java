1811f084042c14e9dca4aaabfc45adbfe5cdd96d
radhikadeshmukh785@gmail.com <161029715+deshmukh30@users.noreply.github.com>
2026-06-10 17:32:48 +0530

    Updated Java code

---
+++ Main.java
@@ -0,0 +1,17 @@
+class Main {
+    public static void main(String[] args) {
+        int age =20;
+        float price = 99.9f;
+        double salary = 5000;
+        char grade = 'A';
+        boolean isPass = true;
+        String name = "Radhika";
+        
+        System.out.println(age);
+        System.out.println(price);
+        System.out.println(salary);
+        System.out.println(grade);
+        System.out.println(isPass);
+        System.out.println(name);
+    }
+}
