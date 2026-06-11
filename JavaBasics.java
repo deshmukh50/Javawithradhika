e6371cbe5108b58f6994fa8f3a56498e597b81d6
radhikadeshmukh785@gmail.com <161029715+deshmukh30@users.noreply.github.com>
2026-06-11 21:56:19 +0530

    Added JavaBasics.java

---
+++ JavaBasics.java
@@ -0,0 +1,10 @@
+public class JavaBasics {
+    public static void main(String[] args) {
+    int a = 10;
+    int b =5;
+    System.out.println(a);
+    System.out.println(b);
+    String Name = "John";
+    System.out.println(Name);
+ }
+}
