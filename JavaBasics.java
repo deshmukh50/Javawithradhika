b38c8ab0d604030ca1c1173798ceff2cb099ed90
radhikadeshmukh785@gmail.com <161029715+deshmukh30@users.noreply.github.com>
2026-06-13 20:50:12 +0530

    Updated Java code

--- JavaBasics.java
+++ JavaBasics.java
@@ -1,10 +1,8 @@
 public class JavaBasics {
     public static void main(String[] args) {
     int a = 10;
-    int b =5;
-    System.out.println(a);
-    System.out.println(b);
-    String Name = "John";
-    System.out.println(Name);
+    int b = 5;
+    int sum = a + b;
+    System.out.println(sum);
  }
 }
